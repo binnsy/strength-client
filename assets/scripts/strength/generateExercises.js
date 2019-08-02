f931ede62a2f46ef33ca6eb10c0d42d8c2dab19c
binnsyGA <alice.binns17@gmail.com>
2019-08-02 16:15:36 -0400

    attempt 100

--- assets/scripts/strength/generateExercises.js
+++ assets/scripts/strength/generateExercises.js
@@ -1,37 +1,37 @@
 // const exercises
 
-let exercises = [
-  '10 burpees, 10 pushups, 25 bicycle crunches',
-  '10 tri dips, 20 split squat jumps, 25 dead bugs',
-  '20 sumo squats, 50 reverse crunches, 10 bent over rows',
-  '20 jump squats, 15 diamond pushups, 1 minute plank',
-  '30 second left side plank, 30 second right side plank, 10 bosu ball leg curls, 20 burpees',
-  '45 second hold wall squat, 20 slow calf raises, 20 dumbbell rows',
-  '15 standing overhead dumbbell presses, single leg dead lift (12 each side), 50 situps',
-  '20 glute bridges, 25 mountain climbers, 24 total walking lunges',
-  '60 russian twists, 10 rotational pushups, 10 deadlifts'
-]
-
-// $(document).ready(function generateExercises () {
-function generateExercises () {
-  const random = (Math.random() * exercises.length) | 0
-  console.log(random)
-  console.log(exercises[random])
-  // const exercise = exercises[random]
-  // console.log(exercise)
-  jQuery('.generateExercisesDisplay').show()
-  document.getElementById('generateExercisesDisplay').innerHTML = exercises[random]
-}
-// jQuery('.generateExercisesDisplay').show()
-// jQuery('#generateExercisesButton').click(function () {
-//   generateExercises()
-//   console.log(exercises)
-// i++;
-// if (i >= x.length) {
-//   i = 0;
-// })
-// })
-document.getElementById('generateExercisesButton').addEventListener('click', generateExercises, false)
-//   document.getElementById('generateExercisesDisplay').innerHTML = exercise
-// $('.generateExercisesDisplay').append(exercises)
-// $('.generateExercisesDisplay').text(exercises[randomNum])
+// let exercises = [
+//   '10 burpees, 10 pushups, 25 bicycle crunches',
+//   '10 tri dips, 20 split squat jumps, 25 dead bugs',
+//   '20 sumo squats, 50 reverse crunches, 10 bent over rows',
+//   '20 jump squats, 15 diamond pushups, 1 minute plank',
+//   '30 second left side plank, 30 second right side plank, 10 bosu ball leg curls, 20 burpees',
+//   '45 second hold wall squat, 20 slow calf raises, 20 dumbbell rows',
+//   '15 standing overhead dumbbell presses, single leg dead lift (12 each side), 50 situps',
+//   '20 glute bridges, 25 mountain climbers, 24 total walking lunges',
+//   '60 russian twists, 10 rotational pushups, 10 deadlifts'
+// ]
+//
+// // $(document).ready(function generateExercises () {
+// function generateExercises () {
+//   const random = (Math.random() * exercises.length) | 0
+//   console.log(random)
+//   console.log(exercises[random])
+//   // const exercise = exercises[random]
+//   // console.log(exercise)
+//   jQuery('.generateExercisesDisplay').show()
+//   document.getElementById('generateExercisesDisplay').innerHTML = exercises[random]
+// }
+// // jQuery('.generateExercisesDisplay').show()
+// // jQuery('#generateExercisesButton').click(function () {
+// //   generateExercises()
+// //   console.log(exercises)
+// // i++;
+// // if (i >= x.length) {
+// //   i = 0;
+// // })
+// // })
+// document.getElementById('generateExercisesButton').addEventListener('click', generateExercises, false)
+// //   document.getElementById('generateExercisesDisplay').innerHTML = exercise
+// // $('.generateExercisesDisplay').append(exercises)
+// // $('.generateExercisesDisplay').text(exercises[randomNum])
